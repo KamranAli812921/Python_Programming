e9a45e0388580228a6e616c8991de06473348b7f
Kamran Ali <kamranroonjha94@gmail.com>
2026-07-01 12:18:22 +0500

    Opens file by with open function

--- 02_fileIO.py
+++ 02_fileIO.py
@@ -1,8 +1,11 @@
 import os
 print(os.getcwd())
-file = open(r'02_file\file.txt','r')
+# file = open(r'02_file\file.txt','r')
 
-for line in file:
-    if 'y' in line:
+# for line in file:
+#     if 'y' in line:
+#         print(line,end="")
+# file.close()
+with open(r'02_file\file.txt','r') as file:
+    for line in file:
         print(line,end="")
-file.close()
