df101671249137e9254064ac2ce672d0606ce475
Kamran Ali <kamranroonjha94@gmail.com>
2026-07-02 10:06:19 +0500

    Sum function

--- 03_function.py
+++ 03_function.py
@@ -3,3 +3,11 @@ def greet(*args):
         print("Hi! "+name)
    
 greet("Kamran","Irfan","Arshad")
+
+def sum(*args):
+    sum=0
+    for number in args:
+        sum+=int(number)
+    return sum
+
+print("The of the numbers is : "+str(sum(1,2,3,10,4,5)))
