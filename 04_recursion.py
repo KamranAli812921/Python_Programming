f01daad048e43c92c02ee4fc3855e27c3f5fb02b
Kamran Ali <kamranroonjha94@gmail.com>
2026-07-02 19:28:44 +0500

    Fabanucci sequence

--- 04_recursion.py
+++ 04_recursion.py
@@ -5,3 +5,11 @@ def recursiveFun(n):
     recursiveFun(n-1)
     print(n)
 recursiveFun(5)
+
+def fib(n):
+    if n<2:
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
+for i in range(6):
+    print(fib(i))
